ccdc686751d6db4185ff24fc7cbeeeaa7a31d2bd
Yiays <yesiateyoursheep@gmail.com>
2021-03-21 00:21:02 +1300

    First working main/config setup for merely 1.0

--- config.py
+++ config.py
@@ -1,25 +1,26 @@
 from configparser import ConfigParser
 from shutil import copy
 from os import path,makedirs
-import time
+import time, pprint
 
+
+""" Loads the config file automatically """
 class Config(ConfigParser):
   path = "config/"
   file = "config/config.ini"
-  template = "config/config.template.ini"
+  template = "config/config.factory.ini"
 
   def __init__(self):
-    super()
+    ConfigParser.__init__(self)
     if not (path.exists(self.path) and path.exists(self.template)):
-      raise Exception("./config or ./config/config.factory.ini missing")
+      raise Exception(f"./{self.path} or ./{self.template} missing")
     if not (path.exists(self.file) and path.isfile(self.file)):
       copy(self.template, self.file)
-    with open(self.file, encoding='utf-8') as f:
-      self.read(f)
+    ConfigParser.read(self, self.file, encoding='utf-8')
   
   def save(self):
     if not path.exists(self.path+'config_history/'+time.strftime("%m-%y")):
       makedirs(self.path+'config_history/'+time.strftime("%m-%y"))
     copy(self.file, self.path+'config_history/'+time.strftime("%m-%y")+'/config-'+time.strftime("%H:%M.%S-%d-%m-%y")+'.ini')
-    with open(self.file, encoding='utf-8') as f:
-      self.write(f)
+    with open(self.file, 'w', encoding='utf-8') as f:
+      ConfigParser.write(self, f)
--- main.py
+++ main.py
@@ -1,47 +1,88 @@
-from discord import Intents
+import discord
 from discord.ext import commands 
-from .config import Config
+from config import Config
 import sys, time, os
 
-#stdout to file
-if not os.path.exists(globals.store+'logs'): os.makedirs(globals.store+'logs')
+class merelybot(commands.Bot):
+	config = Config()
+
+	intents = discord.Intents.none()
+	intents.guilds = True
+	intents.members = False
+	intents.bans = False
+	intents.emojis = False
+	intents.integrations = False
+	intents.webhooks = False
+	intents.invites = False
+	intents.voice_states = False
+	intents.presences = False
+	intents.messages = True
+	# intents.guild_messages
+	# intents.dm_messages
+	intents.reactions = True
+	# intents.guild_reactions
+	# intents.dm_reactions
+	intents.typing = False
+	# intents.guild_typing
+	# intents.dm_typing
+
+	case_insensitive = True
+
+	def __init__(self):
+		print(f"""
+		merelybot{' beta' if self.config['main']['beta'] else ''} v{self.config['main']['ver']}
+		{"(currently named "+self.config['main']['botname']+" by config)" if self.config['main']['botname'] != 'merelybot' else ''}
+		created by Yiays#5930. https://github.com/yiays/merelybot
+		""")
+
+		#stdout to file
+		if not os.path.exists('logs'): os.makedirs('logs')
+
+		sys.stdout = Logger()
+		sys.stderr = Logger(err=True)
+
+		prefixes = ()
+		if self.config['main']['prefix_short']:
+			prefixes += (self.config['main']['prefix_short']+' ', self.config['main']['prefix_short'])
+		if self.config['main']['prefix_long']: prefixes += (self.config['main']['prefix_long']+' ')
+
+		super().__init__(command_prefix = commands.when_mentioned_or(*prefixes),
+										 help_command = None,
+										 intents = self.intents)
+
+		self.autoload_extensions()
+
+	def autoload_extensions(self):
+		for ext in os.listdir('extensions'):
+			if ext[-3:] == '.py':
+				if ext[:-3] in self.config['extensions'].keys():
+					if self.config.getboolean('extensions', ext[:-3]):
+						try:
+							self.load_extension('extensions.'+ext[:-3])
+						except Exception as e:
+							print(f"Failed to load extension '{ext[:-3]}':\n{e}")
+					else:
+						if self.config.getboolean('main','verbose'): print(f"Extension {ext[:-3]} is disabled, skipping.")
+				else:
+					self.config['extensions'][ext[:-3]] = 'False'
+		self.config.save()
+
 class Logger(object):
-	def __init__(self,err=False):
-		self.terminal = sys.stdout
+	def __init__(self, err=False):
+		self.terminal = sys.stderr if err else sys.stdout
 		self.err = err
 	def write(self, message):
 		self.terminal.write(message.encode('utf-8').decode('ascii','ignore'))
-		with open(globals.store+"logs/merely"+('-errors' if self.err else '')+"-"+globals.ver+"-"+time.strftime("%d-%m-%y")+".log", "a", encoding='utf-8') as log:
+		with open("logs/merely"+('-errors' if self.err else '')+"-"+time.strftime("%d-%m-%y")+".log", "a", encoding='utf-8') as log:
 			log.write(message)
 	def flush(self):
 		return self
 
-sys.stdout = Logger()
-sys.stderr = Logger(err=True)
-
-config = Config()
-
-intents = Intents.none()
-intents.guilds = True
-intents.members = False
-intents.bans = False
-intents.emojis = False
-intents.integrations = False
-intents.webhooks = False
-intents.invites = False
-intents.voice_states = False
-intents.presences = False
-intents.messages = True
-# intents.guild_messages
-# intents.dm_messages
-# intents.reactions = True
-# intents.guild_reactions
-# intents.dm_reactions
-intents.typing = False
-# intents.guild_typing
-# intents.dm_typing
-
-
-bot = commands.Bot(command_prefix=config['main']['prefix'])
-bot.config = config
-bot.remove_command('help')
+if __name__ == '__main__':
+	bot = merelybot()
+	try:
+		bot.run(os.environ.get('Merely') if not bot.config.getboolean('main','beta') else os.environ.get('MerelyBeta'))
+	except discord.LoginFailure:
+		raise Exception("failed to login! make sure you provided the correct token using the correct key.")
+	
+	print("exited.")
