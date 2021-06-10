4a1e161b64dbb5d902f3295f56e88650b1e9ceb6
BuildTools <yesiateyoursheep@gmail.com>
2021-06-10 14:44:36 +1200

    Added music module

---
+++ extensions/music.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+musicbot_commands = []
+
+class Music(commands.cog.Cog):
+  """ Promotes a musicbot whenever people try to use this bot for music (unless they already have it) """
+  def __init__(self, bot:commands.Bot):
+    self.bot = bot
+    # ensure config file has required data
+    if not bot.config.has_section('music'):
+      bot.config.add_section('music')
+    if 'music_commands' not in bot.config['music']:
+      bot.config['music']['music_commands'] = 'play,pause,stop'
+    if 'music_companion' not in bot.config['music']:
+      bot.config['music']['music_companion'] = ''
+      raise Exception("music/music_companion must be set in order for the music module to work.")
+    self.music.update(aliases=bot.config['music']['music_commands'].split(','))
+  
+  @commands.command()
+  async def music(self, ctx:commands.Context):
+    if ctx.guild.get_member(int(self.bot.config['music']['music_companion'])) is None:
+      await ctx.reply(self.bot.babel(ctx, 'music', 'promo', invite=f"https://discord.com/oauth2/authorize?client_id={self.bot.config['music']['music_companion']}&scope=bot&permissions=0"))
+    elif ctx.invoked_with == 'music':
+      await ctx.reply(self.bot.babel(ctx, 'music', 'success'))
+
+def setup(bot):
+  bot.add_cog(Music(bot))
