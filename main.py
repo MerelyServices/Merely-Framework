b061f1f3f03d78e51dddd946bfe1bc86ef61e60a
Yiays <yesiateyoursheep@gmail.com>
2023-11-15 17:23:56 +1300

    Downstream fixes from ConfessionBot

--- main.py
+++ main.py
@@ -45,15 +45,19 @@ class MerelyBot(commands.AutoShardedBot):
 		intents.voice_states = self.config.getboolean('intents', 'voice_states')
 		intents.presences = self.config.getboolean('intents', 'presences')
 		intents.message_content = self.config.getboolean('intents', 'message_content')
-		intents.guild_messages = 'guild' in self.config.get('intents', 'messages')
-		intents.dm_messages = 'dm' in self.config.get('intents', 'messages')
+		intents.guild_messages = 'guild' in self.config.get('intents', 'guild_messages')
+		intents.dm_messages = 'dm' in self.config.get('intents', 'dm_messages')
+		intents.messages = intents.guild_messages or intents.dm_messages
 		intents.guild_reactions = 'guild' in self.config.get('intents', 'reactions')
 		intents.dm_reactions = 'dm' in self.config.get('intents', 'reactions')
+		intents.reactions = intents.guild_reactions or intents.dm_reactions
 		intents.guild_typing = 'guild' in self.config.get('intents', 'typing')
 		intents.dm_typing = 'dm' in self.config.get('intents', 'typing')
+		intents.typing = intents.guild_typing or intents.dm_typing
 
 		# set cache policy
-		if self.config.get('intents', 'members') == 'uncached':
+		cachepolicy = disnake.MemberCacheFlags.all()
+		if self.config.get('intents', 'members') in ('uncached', 'False'):
 			cachepolicy = disnake.MemberCacheFlags.none()
 
 		super().__init__(
