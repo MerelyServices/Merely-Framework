15aa71db08cda2671036c8431481a775b193d065
Yiays <yesiateyoursheep@gmail.com>
2024-10-28 17:18:08 +1300

    Improve error handling when /download fails

--- extensions/download.py
+++ extensions/download.py
@@ -96,7 +96,7 @@ class Download(commands.Cog):
     if os.path.exists(filepath):
       await inter.edit_original_response(attachments=(discord.File(filepath),))
     else:
-      await inter.edit_original_response(self.babel(inter, 'failed', log=logs))
+      await inter.edit_original_response(content=self.babel(inter, 'failed', log=logs))
 
 
 async def setup(bot:MerelyBot):
